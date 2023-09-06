9cea25e9527b4c38328f51001b653f86d916765c
selivanova <selivanova@>
2023-09-06 08:26:00 +0300

    Add file

--- main.py
+++ main.py
@@ -65,10 +65,11 @@ goods12 = pd.pivot_table(goods4, index=['DATA'], columns=['TYPE MOVEMENT'], valu
 
 #ABC-анализ
 goods_ABC = pd.pivot_table(goods1, index=['ITEM NUMBER'], values=['SUM'], aggfunc=nd.sum).sort_values(by=['SUM'], ascending=False)
+#Определение доли
 goods_ABC['SUM_p'] = goods_ABC['SUM'] / goods_ABC['SUM'].sum()
 goods_ABC['SUM_p'] = goods_ABC['SUM_p'].apply(lambda x: round(x * 100, 2))
 goods_ABC = goods_ABC.sort_values(by='SUM_p', ascending=False)
-
+#Определение накопительной доли
 goods_ABC['SUM_p1'] = goods_ABC['SUM_p'].cumsum()
 
 goods_ABC['ABC_group'] = goods_ABC['SUM_p1'].apply(lambda abc:
@@ -136,7 +137,7 @@ goods_sheets = {
     'ABC_XYZ': goods_ABC_XYZ2,
     'Потребность запас': goods_reserve,
 }
-writer = pd.ExcelWriter(r'C:\Users\selivanova\Desktop\HM Python\ProjectPP\Сводные.xlsx', engine='xlsxwriter')
+writer = pd.ExcelWriter(r'C:\Users\***\Desktop\HM Python\ProjectPP\Сводные.xlsx', engine='xlsxwriter')
 for sheet_name in goods_sheets.keys():
     goods_sheets[sheet_name].to_excel(writer, sheet_name=sheet_name, index=True)
 
@@ -198,38 +199,38 @@ def main():
 
     #plt
     # Продажи по дням товара с max выручкой
-    goods11.plot(kind='bar')
-    plt.title('Продажи по дням товара с max выручкой')
-    plt.xticks(rotation=45)
-    plt.show()
+    #goods11.plot(kind='bar')
+    #plt.title('Продажи по дням товара с max выручкой')
+    #plt.xticks(rotation=45)
+    #plt.show()
 
     # Списания товара по дням
-    goods12.plot(kind='barh')
-    plt.title('Списания товара по дням')
-    plt.xticks(rotation=45)
-    plt.show()
+    #goods12.plot(kind='barh')
+    #plt.title('Списания товара по дням')
+    #plt.xticks(rotation=45)
+    #plt.show()
 
     # ABC-анализ
-    plt.plot(goods_ABC['SUM_p'].cumsum().head(5))
-    plt.title('Кривая Парето (кривая Лоренца или ABC-кривая)')
-    plt.show()
+    #plt.plot(goods_ABC['SUM_p'].cumsum().head(5))
+    #plt.title('Кривая Парето (кривая Лоренца или ABC-кривая)')
+    #plt.show()
 
     #Применение кластеризации
     #Метка графика 0
-    plt.scatter(filtered_label0[:, 0], filtered_label0[:, 1])
-    plt.title('Метка графика 0')
-    plt.show()
+    #plt.scatter(filtered_label0[:, 0], filtered_label0[:, 1])
+    #plt.title('Метка графика 0')
+    #plt.show()
     #Нанесение меток 2 и 8
-    plt.scatter(filtered_label2[:, 0], filtered_label2[:, 1], color='red')
-    plt.scatter(filtered_label8[:, 0], filtered_label8[:, 1], color='black')
-    plt.title('Нанесение меток 2 и 8')
-    plt.show()
+    #plt.scatter(filtered_label2[:, 0], filtered_label2[:, 1], color='red')
+    #plt.scatter(filtered_label8[:, 0], filtered_label8[:, 1], color='black')
+    #plt.title('Нанесение меток 2 и 8')
+    #plt.show()
     #Заключительные кластеры
-    for i in u_labels:
-        plt.scatter(df[label == i, 0], df[label == i, 1], label=i)
-    plt.legend()
-    plt.title('Заключительные кластеры')
-    plt.show()
+    #for i in u_labels:
+        #plt.scatter(df[label == i, 0], df[label == i, 1], label=i)
+    #plt.legend()
+    #plt.title('Заключительные кластеры')
+    #plt.show()
 
 if __name__ == '__main__':
     main()
